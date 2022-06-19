e34fdaa098217344fcd2c0222f3c92a465ad34c5
Harsimran Kaur <72742424+Harsimrank23@users.noreply.github.com>
2022-06-19 18:32:06 +0530

    Time: 92 ms (68.40%), Space: 29.5 MB (73.24%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        string current="";
+        vector<string>temp;
+        vector<vector<string>>ans;
+        sort(products.begin(),products.end());
+        
+        for(auto c:searchWord)
+        {
+            current+=c;
+            temp.clear();
+            auto itr=lower_bound(products.begin(),products.end(),current);
+            
+            for(int i=0;i<3 && itr+i!=products.end();i++)
+            {
+                string now=*(itr+i);
+                if(now.find(current))
+                    break;
+                temp.push_back(now);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
