00c9c5a64d844712d76b3d7644ca467843615d5f
Harsimran Kaur <72742424+Harsimrank23@users.noreply.github.com>
2022-07-01 14:47:28 +0530

    Time: 52 ms (22.68%), Space: 121.2 MB (19.46%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n==1)
+            return "1";
+        
+        // recursion:
+        string s=countAndSay(n-1);
+        
+        string res=""; 
+        
+        // find frequency:
+        int counter=0;
+        
+        for(int i=0;i<s.length();i++)
+        {
+            counter++;
+            if(i==s.length()-1 || s[i]!=s[i+1])
+            {
+                res=res+to_string(counter)+s[i];
+                counter=0;
+            }
+        }
+        return res;
+    }
+};
